f3ea995574d646544ec6a530d048f07973c6981d
junhyun87 <pe.jeon87@frentree.com>
2019-08-08 13:22:29 +0900

    Why so serious - Final

---
+++ app/build/tmp/kapt3/stubs/debug/com/raywenderlich/android/whysoserious/ui/profile/ProfileFragment.java
@@ -0,0 +1,53 @@
+package com.raywenderlich.android.whysoserious.ui.profile;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 1, 9}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000H\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010\u000e\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0003\u0018\u00002\u00020\u00012\u00020\u0002B\u0005\u00a2\u0006\u0002\u0010\u0003J\b\u0010\n\u001a\u00020\u000bH\u0002J&\u0010\f\u001a\u0004\u0018\u00010\r2\u0006\u0010\u000e\u001a\u00020\u000f2\b\u0010\u0010\u001a\u0004\u0018\u00010\u00112\b\u0010\u0012\u001a\u0004\u0018\u00010\u0013H\u0016J\u001a\u0010\u0014\u001a\u00020\u000b2\u0006\u0010\u0015\u001a\u00020\r2\b\u0010\u0012\u001a\u0004\u0018\u00010\u0013H\u0016J\b\u0010\u0016\u001a\u00020\u000bH\u0016J\u0010\u0010\u0017\u001a\u00020\u000b2\u0006\u0010\u0018\u001a\u00020\u0019H\u0016J\u0010\u0010\u001a\u001a\u00020\u000b2\u0006\u0010\u001b\u001a\u00020\u001cH\u0016J\u0010\u0010\u001d\u001a\u00020\u000b2\u0006\u0010\u001e\u001a\u00020\u0019H\u0016R\u001b\u0010\u0004\u001a\u00020\u00058BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\b\u0010\t\u001a\u0004\b\u0006\u0010\u0007\u00a8\u0006\u001f"}, d2 = {"Lcom/raywenderlich/android/whysoserious/ui/profile/ProfileFragment;", "Landroid/support/v4/app/Fragment;", "Lcom/raywenderlich/android/whysoserious/ui/profile/ProfileView;", "()V", "presenter", "Lcom/raywenderlich/android/whysoserious/presentation/ProfilePresenter;", "getPresenter", "()Lcom/raywenderlich/android/whysoserious/presentation/ProfilePresenter;", "presenter$delegate", "Lkotlin/Lazy;", "initUi", "", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onViewCreated", "view", "openWelcome", "showEmail", "email", "", "showNumberOfJokes", "jokesCount", "", "showUsername", "username", "app_debug"})
+public final class ProfileFragment extends android.support.v4.app.Fragment implements com.raywenderlich.android.whysoserious.ui.profile.ProfileView {
+    private final kotlin.Lazy presenter$delegate = null;
+    private java.util.HashMap _$_findViewCache;
+    
+    private final com.raywenderlich.android.whysoserious.presentation.ProfilePresenter getPresenter() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    private final void initUi() {
+    }
+    
+    @java.lang.Override()
+    public void showUsername(@org.jetbrains.annotations.NotNull()
+    java.lang.String username) {
+    }
+    
+    @java.lang.Override()
+    public void showEmail(@org.jetbrains.annotations.NotNull()
+    java.lang.String email) {
+    }
+    
+    @java.lang.Override()
+    public void showNumberOfJokes(int jokesCount) {
+    }
+    
+    @java.lang.Override()
+    public void openWelcome() {
+    }
+    
+    public ProfileFragment() {
+        super();
+    }
+}
